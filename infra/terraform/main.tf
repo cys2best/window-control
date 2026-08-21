43623f8d3c90643607dbf57fde2ae9d04d9fea5f
cysbest <tuananhngo1268@gmail.com>
2026-08-21 21:24:41 +0700

    feat(terraform): open 443/80 for public UI tunnel TLS

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -80,6 +80,22 @@ resource "aws_security_group" "webrtc_poc" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    description = "Public UI tunnel HTTPS (nginx TLS termination)"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP (certbot ACME challenge + HTTPS redirect)"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
