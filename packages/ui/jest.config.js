ff4c9cc9bff667e1f4d39cedfe21fe86ef53aae1
cysbest <tuananhngo1268@gmail.com>
2026-09-05 02:30:35 +0700

    feat(ui): extract shared theme and primitive components into @wc/ui

--- packages/ui/jest.config.js
+++ packages/ui/jest.config.js
@@ -1,4 +1,6 @@
 module.exports = {
-  preset: "ts-jest",
-  testEnvironment: "jsdom",
+  preset: "jest-expo",
+  transformIgnorePatterns: [
+    "node_modules/(?!((jest-)?react-native|@react-native(-community)?|expo(nent)?|expo-.*|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|react-native-webrtc|react-native-gesture-handler|react-native-reanimated)/)",
+  ],
 };
--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,1 +1,14 @@
 export const UI_PACKAGE_READY = true;
+
+export * from "./theme/tokens";
+export * from "./components/Button";
+export * from "./components/IconButton";
+export * from "./components/NetChip";
+export * from "./components/NetDot";
+export * from "./components/StatsOverlay";
+export * from "./components/ErrorOverlay";
+export * from "./components/BottomNav";
+export * from "./components/InstanceRow";
+export * from "./components/StreamToolbar";
+export * from "./components/SwitchDrawer";
+export * from "./components/SettingsModal";
